537c4fd416e932f57e88445cc105816991a1565b
ibktommy <atomquartz@gmail.com>
2022-10-18 12:10:45 +0100

    Add elements and styles to the Home Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home/Home.jsx";
+
 function App() {
 	return (
-    <div className="container">
-      <Routes>
-        <Route></Route>
-      </Routes>
-    </div>
-  )
+		<div className="container">
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<main id="home">
+			<h1 className="header">Welcome to React Chat WebApp</h1>
+
+			<form className="home-form">
+				<input type="text" placeholder="Enter Your Username" />
+				<button type="submit">Join Chat</button>
+			</form>
+		</main>
+	);
 };
 
 export default Home;
