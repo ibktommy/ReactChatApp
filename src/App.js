efba4a16318ee227c7aa9df9248545f8cf7dbab6
ibktommy <atomquartz@gmail.com>
2022-10-18 12:55:49 +0100

    Adding elements and styles to the Chat-Component-Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home/Home.jsx";
+import Chat from "./pages/Chat/Chat.jsx";
 
 function App() {
 	return (
 		<div className="container">
 			<Routes>
 				<Route exact path="/" element={<Home />} />
+				<Route path="/chat" element={<Chat />} />
 			</Routes>
 		</div>
 	);
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import "./Chat.css";
 
 const Chat = () => {
-	return <div>Chat</div>;
+	return (
+		<main id="chat">
+			<div className="title">Chat Room</div>
+			<div className="chat-body">
+				<p className="welcome">Atom, Welcome to the chat room</p>
+				<article className="message">
+					<p className="user-message">Hello there,hpesqvqxk</p>
+					<p className="user-name">Atom</p>
+				</article>
+			</div>
+			<div className="chat-actions">
+				<input type="text" placeholder="Type a message..." />
+				<button>Send</button>
+			</div>
+		</main>
+	);
 };
 
 export default Chat;
