f9f33f6dee8563a6707d5789953f26907c00f90e
ibktommy <atomquartz@gmail.com>
2022-10-19 21:42:34 +0100

    Create page

---
+++ src/components/MessageText.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MessageText = () => {
+	return <div>MessageText</div>;
+};
+
+export default MessageText;
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Chat.css";
+import formatText from "../../utilities/utils";
 
 const Chat = () => {
-	return <h1>Chat Room</h1>;
+	return (
+		<main id="chat">
+			<div className="title">Chat Room</div>
+
+			<div className="chat-body">
+				<p className="welcome">Atomdev, Welcome to the chat room</p>
+			</div>
+
+			<form className="chat-actions">
+				<input type="text" placeholder="Type a message..." />
+				<button type="submit">Send</button>
+			</form>
+		</main>
+	);
 };
 
 export default Chat;
