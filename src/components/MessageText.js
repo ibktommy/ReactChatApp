ede2bcd8f47032f422c3ea8fdda3f73153fffe55
ibktommy <atomquartz@gmail.com>
2022-10-26 10:56:11 +0100

    Mapped through userMessages to display messages of users having a standard UI

--- src/components/MessageText.js
+++ src/components/MessageText.js
@@ -9,7 +9,87 @@ const MessageText = () => {
 	const userMessages = JSON.parse(localStorage.getItem("usersMessages"));
 
 	return (
-		<div className="message-text">
+		<>
+			{userMessages &&
+				userMessages.map((messageItem) => {
+					let { id, user, message } = messageItem;
+
+					if (id === currentUserID) {
+						return (
+							<div className="box">
+								<div className="sent">
+									<span className="main-msg">{message}</span>
+									<span className="username">
+										{formatText(user).slice(0, 6)}
+									</span>
+								</div>
+								<div className="receive"></div>
+							</div>
+						);
+					} else {
+						return (
+							<div className="box">
+								<div className="sent"></div>
+								<div className="receive">
+									<span className="other-msg">{message}</span>
+									<span className="username">
+										{formatText(user).slice(0, 6)}
+									</span>
+								</div>
+							</div>
+						);
+					}
+				})}
+		</>
+	);
+};
+
+export default MessageText;
+
+// {
+// 	userMessages &&
+// 		userMessages.map((messageItem, index) => {
+// 			let { id } = messageItem;
+// 			return (
+// 				<div className="box" key={index}>
+// 					{id === currentUserID
+// 						? userMessages
+// 								.filter((item) => item.id === currentUserID)
+// 								.map((item) => {
+// 									let { user, message, index } = item;
+// 									return (
+// 										<div className="sent" key={index}>
+// 											<span className="main-msg">{message}</span>
+// 											<span className="username">{user}</span>
+// 										</div>
+// 									);
+// 								})
+// 						: userMessages
+// 								.filter((item) => item.id !== currentUserID)
+// 								.map((item) => {
+// 									let { user, message, index } = item;
+// 									return (
+// 										<div className="receive" key={index}>
+// 											<span className="other-msg">{message}</span>
+// 											<span className="username">{user}</span>
+// 										</div>
+// 									);
+// 								})}
+// 				</div>
+// 			);
+// 		});
+// }
+
+// let { user, message, index } = item;
+// return (
+// 	<div className="sent" key={index}>
+// 		<span className="main-msg">{message}</span>
+// 		<span className="username">{user}</span>
+// 	</div>
+// );
+
+{
+	/* <div className="message-text">
 			<div className="mainUser">
 				<div>
 					{userMessages &&
@@ -55,9 +135,6 @@ const MessageText = () => {
 							})}
 				</div>
 			</div>
-		</div>
-	);
-};
-
-export default MessageText;
+		</div> */
+}
 
